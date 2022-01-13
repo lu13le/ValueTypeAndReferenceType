6fd1fefa2d20e3bacef6f3d921da3201d7c2ebc2
Luka Raicevic <raicevic12345@gmail.com>
2022-01-13 16:26:06 +0100

    Add project files.

---
+++ ValueTypeAndReferenceType/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ValueTypeAndReferenceType
+{
+    internal class Program
+    {
+        static void promeniVrednostiTip(int a)
+        {
+            a = 1998;
+            Console.WriteLine(a);
+        }
+
+        static void promeniReferentniTip(Team teamA)
+        {
+            teamA.Name = "Lakers";
+        }
+
+        static void Main(string[] args)
+        {
+            int b = 2022;
+            Console.WriteLine(b);
+            promeniVrednostiTip(b);
+            Console.WriteLine(b);
+
+            Team teamB = new Team();
+            teamB.Name = "Golden State";
+
+            promeniReferentniTip(teamB);
+
+            Console.WriteLine(teamB.Name);        }
+    }
+}
---
+++ ValueTypeAndReferenceType/Team.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValueTypeAndReferenceType
+{
+    internal class Team
+    {
+        public string Name { get; set; }    
+    }
+}
